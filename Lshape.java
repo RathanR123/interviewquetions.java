76b4bc2815c723dba7a43ceb8d0578d0941188f5
RathanR123 <137428872+RathanR123@users.noreply.github.com>
2024-06-29 13:09:53 +0530

    Add files via upload

---
+++ Lshape.java
@@ -0,0 +1,13 @@
+public class Lshape {
+    public static void main(String[] args) {
+        int size=5;
+        for(int i=0;i<size-1;i++){
+                System.out.println("*");
+            }
+        for(int i=0;i<size;i++){
+                System.out.print("* ");
+        }
+        System.out.println();
+
+    }
+}
